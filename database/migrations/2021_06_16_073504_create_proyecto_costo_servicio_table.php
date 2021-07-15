108341f54868d220cc135fc31b92eaae5b993045
Eliud Niebles Lopez <eliudniebles@gmail.com>
2021-07-15 18:13:02 -0500

    correcion en costo servicio2

--- database/migrations/2021_06_16_073504_create_proyecto_costo_servicio_table.php
+++ database/migrations/2021_06_16_073504_create_proyecto_costo_servicio_table.php
@@ -21,7 +21,7 @@ class  CreateProyectoCostoServicioTable extends Migration
             $table->enum('forma_pago',
             ['Diaria','Semanal','Quincenal','Mensual']);
             $table->enum('medio_pago',[
-                'Efectivo','Tranferencia','Credito','Otros'
+                'Efectivo','Transferencia','Credito','Otros'
             ]);
             $table->string('otro_medio_pago')->nullable();
             $table->enum('pago_a_realizar',[
